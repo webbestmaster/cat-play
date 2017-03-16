c8266fb5a99f627ab766dd2dff04648a30093423
webbestmaster <web.best.master@gmail.com>
2017-03-16 09:53:20 +0300

    refactoring

--- www/component/game-header/view.js
+++ www/component/game-header/view.js
@@ -1,14 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import setText from './action/setText'
-import { hashHistory } from 'react-router';
+import { withRouter } from 'react-router';
 
 class GameHeader extends Component {
 
     render() {
-
         return <div className="game-header">
-            <div className="game-header__back-button" onClick={() => hashHistory.goBack() }/>
+            <div className="game-header__back-button" onClick={() => this.props.router.goBack() }/>
             <h2 className="game-header__header">{this.props.headerTextReducer.text}</h2>
         </div>;
 
@@ -23,4 +22,4 @@ export default connect(
     {
         setTextAction: setText
     }
-)(GameHeader);
+)(withRouter(GameHeader));
