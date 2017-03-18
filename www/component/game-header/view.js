0d9521d50610c4a1656f7b5734113da3f31fc216
webbestmaster <web.best.master@gmail.com>
2017-03-18 13:47:56 +0300

    game header - add animation

--- www/component/game-header/view.js
+++ www/component/game-header/view.js
@@ -21,10 +21,35 @@ class GameHeader extends Component {
 
     }
 
+    componentDidUpdate(prevProps, prevState) {
+
+        const view = this;
+        const prevText = prevProps.headerTextReducer.text;
+        const currentText = view.props.headerTextReducer.text;
+        const tweenTime = appConst.tween.time;
+
+        if (prevText !== currentText) {
+
+            const header = view.refs.header;
+            const mySplitText = new SplitText(header, {type: 'words, chars'});
+            const chars = mySplitText.chars;
+            const tl = new TimelineLite();
+
+            header.textContent = prevText;
+
+            tl.to(header, tweenTime, {y: '-100%'})
+                .call(() => header.textContent = currentText)
+                .to(header, tweenTime, {y: '0%'})
+                .call(() => tl.kill());
+
+        }
+
+    }
+
     render() {
         return <div ref="wrapper" className="game-header">
             <div className="game-header__back-button" onClick={() => this.props.router.goBack() }/>
-            <h2 className="game-header__header">{this.props.headerTextReducer.text}</h2>
+            <h2 ref="header" className="game-header__header">{this.props.headerTextReducer.text}</h2>
         </div>;
 
     }
